af568f7ed0b3fb10e89c45b88f6882ee6ee67b9b
Guilherme Ribeiro <xxguilhermesoaresxx@gmail.com>
2023-04-08 17:53:52 -0300

    Create initial configuration

--- Cube/index.js
+++ Cube/index.js
@@ -1,1 +1,27 @@
-console.log(THREE)
+//Criando uma cena
+const scene = new THREE.Scene();
+
+//Criando um objeto/mesh
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 'blue' })
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+//Criando uma câmera
+const aspect = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+const camera = new THREE.PerspectiveCamera(45, aspect.width / aspect.height, 1, 1000 ); //FOV, ASPECT RATIO, NEAR, FAR
+scene.add(camera);
+
+//Afastando a camera
+camera.position.z = 3;
+
+//Criando o renderer
+const canvas = document.getElementById('canvas');
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+//Setando o tamanho da imagem renderizada
+renderer.setSize(aspect.width, aspect.height);
+renderer.render(scene, camera);
